2e6a4219fe792e1426690eab19679e0bbf91219d
Daniel Ecer <de-code@users.noreply.github.com>
2022-02-21 17:38:32 +0000

    renamed to task_status for consitency

--- scripts/wait-for-cloudwatch-task-completion.sh
+++ scripts/wait-for-cloudwatch-task-completion.sh
@@ -5,13 +5,13 @@ set -euo pipefail
 cloudwatch_task_id=$1
 
 while true; do
-    taskStatus=$(aws logs describe-export-tasks --task-id=$cloudwatch_task_id \
+    task_status=$(aws logs describe-export-tasks --task-id=$cloudwatch_task_id \
         | jq -r '.exportTasks[0].status.code')
-    echo $taskStatus
-    if [[ $taskStatus == COMPLETED ]]; then
+    echo $task_status
+    if [[ $task_status == COMPLETED ]]; then
         break
     fi
-    if [[ $taskStatus == FAILED ]]; then
+    if [[ $task_status == FAILED ]]; then
         exit 1
     fi
     sleep 10
