9c057ed099368e597edc5c3188f85f22908fc83b
Daniel Ecer <de-code@users.noreply.github.com>
2022-06-20 13:55:22 +0100

    introduced target file variable for more clarity

--- scripts/convert-gzipped-cloudwatch-logs-to-bigquery-jsonl-gz.sh
+++ scripts/convert-gzipped-cloudwatch-logs-to-bigquery-jsonl-gz.sh
@@ -3,9 +3,11 @@
 set -euo pipefail
 
 log_gz_file="$1"
+common_filename=$(dirname "$log_gz_file")/$(basename "$log_gz_file" .gz)
+target_jsonl_gz_file="$common_filename.jsonl.gz"
 
 zcat $log_gz_file \
     | sed -e 's/[^ ]* //' \
     | jq --compact-output 'del(.kubernetes) | del(.docker)' \
     | gzip - \
-    > $(dirname "$log_gz_file")/$(basename "$log_gz_file" .gz).jsonl.gz
+    > $target_jsonl_gz_file
