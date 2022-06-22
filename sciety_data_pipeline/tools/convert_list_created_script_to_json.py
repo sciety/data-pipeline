22fc5314fb97df71ba67a3e311db16fa57e7fbdb
Daniel Ecer <de-code@users.noreply.github.com>
2022-06-22 15:48:20 +0100

    tool to parse listId from list created events

---
+++ sciety_data_pipeline/tools/convert_list_created_script_to_json.py
@@ -0,0 +1,42 @@
+import argparse
+import logging
+import json
+import re
+from pathlib import Path
+from typing import Optional, Sequence
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def parse_args(argv: Optional[Sequence[str]]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser('Convert listCreated script to JSON')
+    parser.add_argument('--js-script')
+    parser.add_argument('--output-json-file')
+    return parser.parse_args(argv)
+
+
+def iter_parse_list_created_script_content_to_json(js_script_content: str) -> Sequence[dict]:
+    for m in re.finditer(
+        r"listCreated\(\s*[^']+'([^']+)'",
+        js_script_content
+    ):
+        LOGGER.debug('m: %r', m)
+        LOGGER.debug('m: %r', m.group(1))
+        yield {
+            'listId': m.group(1)
+        }
+
+
+def main(argv: Optional[Sequence[str]] = None):
+    args = parse_args(argv)
+    LOGGER.info('args: %r', args)
+    input_file = Path(args.js_script)
+    output_file = Path(args.output_json_file)
+    json_list = list(iter_parse_list_created_script_content_to_json(input_file.read_text()))
+    output_file.write_text(json.dumps(json_list, indent=2))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level='INFO')
+    main()
